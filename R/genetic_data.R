294076a7945f1b7e29e487026197a4755c3a7ad4
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2025-01-08 17:45:00 +0000

    Discretise the phylogenetic tree into slices the same as the prevalence.

---
+++ R/genetic_data.R
@@ -0,0 +1,64 @@
+#' Genetic Data
+#'
+#' Discretises a phylogenetic tree.
+#'
+#' @param ptree object of class phylo.
+#' @param stop_time number of times the epidemic simulation was run for.
+#' @param time number of time points in the past the most recent leaf was sampled.
+#'
+#' @return data frame with 3 columns and N+1 rows: Column 1 is time, column 2 is the number of lineages at the end of that time, column 3 is the number of coalescences on that time.
+#' @export
+#'
+#' @examples
+#' genetic_data(ptree = sample_tree, stop_time = 50, time = 3)
+genetic_data <- function(ptree, stop_time, time = 0) {
+  if (is.null(ptree)) {
+    return(NULL)
+  }
+
+  t <- seq(0, stop_time, by=1)
+  l <- stop_time + 1 #length of epidemic
+
+  #time is 0:n_times
+  data <- list("time" = t, "n_anc" = 0, "n_coal" = NA)
+
+  ltt <- ape::ltt.plot.coords(ptree) #gives number of lineages through time
+  ltt <- list("time"=ltt[,1],"N"=ltt[,2]) #make a list of time and number of lineages
+  ltt$time <- stop_time + ltt$time #forwards in time
+  if (ltt$time[1]==0) {
+    ltt$time <- ltt$time[-1]
+    ltt$N <- ltt$N[-1]
+  }
+  if (time > 0) {
+    ltt$time <- c(ltt$time - time, stop_time)
+    ltt$N <- c(ltt$N, 0)
+  }
+
+  ltt$time <- round(ltt$time, 10)
+
+  #number of lineages through time
+  for (i in 1:l) {
+    #the first occasion where ltt$time is larger than t[i]
+    j <- which.max(ltt$time >= t[i])
+    #at that time, the number of lineages is ltt$N
+    data$n_anc[i] <- ltt$N[j]
+  }
+
+  #coalescence times
+  coal_times <- c()
+  j <- 0
+  for (i in 1:(length(ltt$time)-1)) {
+    if (ltt$N[i] < ltt$N[i+1]) {
+      j <- j + 1
+      coal_times[j] <- ltt$time[i]
+    }
+  }
+
+  #number of coalescences per time
+  for (i in 2:l) {
+    data$n_coal[i] <- sum(coal_times>=t[i-1] & coal_times<t[i])
+  }
+
+  data <- as.data.frame(data)
+  return(data)
+}
